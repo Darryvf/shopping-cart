f32120a5f925c308a292c8e8dc509b87f299b957
Darrell VanFleet <dasmvanfleet@msn.com>
2018-07-18 15:02:50 -0600

    compute total price is working

--- src/items.js
+++ src/items.js
@@ -2,6 +2,7 @@ import React from 'react'
 import CartItem from './CartItem'
 
 const CartItems = (props) => {
+
   return (
     <div className="container">
       <h1>Cart Items</h1>
@@ -14,26 +15,24 @@ const CartItems = (props) => {
           </div>
         </div>
           {props.cartList.map(element => {
-            return <CartItem
+          // { alert(arr) }
+            return (
+              <CartItem
                       key={element.id}
                       name={element.product.name}
                       quantity={element.quantity}
                       price= {element.product.priceInCents}
-                    />
+                    />)
                   })
           }
-      </div>
-      <div>
-      <h2>Total Price:
-  
-          {
-            props.cartList.reduce (
-            (accumulator, currentValue) => accumulator + currentValue.product.priceInCents )
-         }
-      </h2>
+          Total Price:{
 
-
+                    props.cartList.reduce(
+                    (accumulator, element) =>
+                      accumulator + element.product.priceInCents * element.quantity,0)
+            }
       </div>
+
     </div>
   )
 }
